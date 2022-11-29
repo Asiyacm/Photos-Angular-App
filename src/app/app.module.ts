dd18ecb9030e1d41e3f5418b545ac19adcc32ad8
Asiyacm <asiyacmuhammed@gmail.com>
2022-11-29 15:25:19 +0530

    view commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoViewComponent } from './photo-view/photo-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoViewComponent
   ],
   imports: [
     BrowserModule,
